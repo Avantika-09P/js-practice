6b360abdba14651ce11de6b322bf2b30de339b11
Avantika Padhi <avantikapadhi4822@gmail.com>
2025-06-24 15:26:24 +0000

    commit

--- 01_Basics/easy3.js
+++ 01_Basics/easy3.js
@@ -1,3 +1,20 @@
 // Question:
 // Write a function getNames that takes an array of objects (each with a name property) and returns an array of the names.
 
+const myArray = [
+    {name : "Avantika", age: 20, job: "Developer"},
+    {name: "Karan", age:22, job: "Captain"}
+
+];
+
+// function getNames(arr) {
+//     return arr.map(person => person.name) ;
+// }    
+
+//Another function definition
+
+const getNames = (arr) => {
+    return arr.map(person => person.name) ;
+}
+
+console.log(getNames(myArray));
