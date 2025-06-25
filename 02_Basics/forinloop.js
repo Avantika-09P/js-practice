acad3c54e485131e10767cb29bf0118c7c26841b
Avantika Padhi <avantikapadhi4822@gmail.com>
2025-06-25 16:36:59 +0000

    commit

--- 02_Basics/forinloop.js
+++ 02_Basics/forinloop.js
@@ -5,7 +5,18 @@ const myObj = {
     swift: "Swift for Apple"
 };
 
-for (const key in object) {
-    console.log(key);
+for (const key in myObj) {
+    // console.log(`${key} :- ${myObj[key]}`);
     
 }
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    // console.log(key);
+}
+for (const key in programming) {
+    console.log(programming[key]);
+    
+}
+
