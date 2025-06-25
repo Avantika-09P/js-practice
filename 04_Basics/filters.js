67490a4ae41f5e1ee9a2dd2d7878b52ac590938c
Avantika Padhi <avantikapadhi4822@gmail.com>
2025-06-25 17:25:02 +0000

    commit

---
+++ 04_Basics/filters.js
@@ -0,0 +1,13 @@
+const nums =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numsFilter = nums.filter((val) => (val>5));
+// console.log(numsFilter);
+
+
+//2nd method
+const numsFil = nums.filter((val) => {
+    return val>5;
+})
+// console.log(numsFil)
+
+
+
